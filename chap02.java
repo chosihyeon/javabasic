935ae5d2fe53eabb96db14b787a3c4bc99a51185
chosihyeon <162430886+chosihyeon@users.noreply.github.com>
2024-03-06 18:07:43 +0900

    Add files via upload

---
+++ chap02.java
@@ -0,0 +1,18 @@
+package sec04;
+
+public class chap02 {
+
+	public static void main(String[] args) {
+		String myName = "홍길동";
+		int myAge = 20;
+		
+		System.out.println(" 이름 : " + myName);
+		System.out.println(" 나이 : " + myAge);
+		System.out.println();
+		//System.out.print(" 이름 : " + myName);
+		//System.out.print(" 이름 : " + myName);
+		System.out.println(" 이름 : " + myName +"	 나이 :" + myAge);
+//오른쪽 실행 결과처럼 보이게 코드 작성s
+	}
+
+}
