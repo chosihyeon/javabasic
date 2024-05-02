91fdfacaab3cb2ac768411c0d455dfc6e730fe79
chosihyeon <162430886+chosihyeon@users.noreply.github.com>
2024-05-02 20:43:51 +0900

    Update Girl.java

--- _0502/C3/Girl.java
+++ _0502/C3/Girl.java
@@ -1,6 +1,14 @@
 package C3;
 
-public class Girl {
+public class GirlTest {
+
+	public static void main(String[] args) {
+		Girl[]girls = {new Girl ("갑순이"), new GoodGirl("콩쥐"),new BestGirl("황진이")};
+		for(Girl g :girls)
+			g.show();
+	}
+
+ class Girl {
 //필드
 	public Girl(String name) {
 	this.name =name;	
